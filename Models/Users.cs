72deb8c069a11d6a7bde17f940e1c2f832e3e33f
Britnay Gore <britnaygore268@gmail.com>
2024-08-06 19:49:49 -0400

    Made ProfileImageUrl a nullable value

--- Models/Users.cs
+++ Models/Users.cs
@@ -12,10 +12,10 @@ public class Users
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public string Bio { get; set; }
+    public string? Bio { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public string ProfileImageUrl { get; set; }
+    public string? ProfileImageUrl { get; set; }
     public DateTime CreatedOn { get; set; }
     public bool Active { get; set; }
 }
