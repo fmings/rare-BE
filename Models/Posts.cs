ee80caae94276892a58a0023c69ce11219361a5f
fmings <mingsfelicia@gmail.com>
2024-08-06 18:30:47 -0500

    added class details for Comments & Posts classes

--- Models/Posts.cs
+++ Models/Posts.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,15 @@ namespace rare_BE.Models
 {
     public class Posts
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Content { get; set; }
+        public bool Approved { get; set; }
+
+
     }
 }
