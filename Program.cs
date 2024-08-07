b54761d7bcf03e7c9ebfc6db5b6b35eb94236ba7
fmings <mingsfelicia@gmail.com>
2024-08-06 19:58:37 -0500

    added mock data for comments and posts

--- Program.cs
+++ Program.cs
@@ -14,6 +14,50 @@ List<Posts> posts = new List<Posts>()
         Content = "Hello World, how are you today?",
         Approved = true
     },
+    new Posts()
+    {
+        Id = 2,
+        UserId = 2,
+        CategoryId = 2,
+        Title = "Hello Zach",
+        PublicationDate = new DateTime(2024, 8, 5),
+        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/e7/Zach_Galifianakis_2012_%28cropped%29.jpg",
+        Content = "Hello Zach, doing well?",
+        Approved = false
+    },
+    new Posts()
+    {
+        Id = 3,
+        UserId = 3,
+        CategoryId = 3,
+        Title = "Hello Brit",
+        PublicationDate = new DateTime(2024, 6, 23),
+        ImageUrl = "https://www.rollingstone.com/wp-content/uploads/2022/02/AdeleBritsRedCarpet-1.jpeg",
+        Content = "Hello Brit, how's work?",
+        Approved = true
+    },
+    new Posts()
+    {
+        Id = 4,
+        UserId = 4,
+        CategoryId = 4,
+        Title = "Hello Fletcher",
+        PublicationDate = new DateTime(2024, 1, 1),
+        ImageUrl = "https://www.denofgeek.com/wp-content/uploads/2020/08/chevy-chase-fletch-universal-pictures.jpg?fit=1200%2C680",
+        Content = "Hello Fletcher, you are turning 21 this year!",
+        Approved = true
+    },
+    new Posts()
+    {
+        Id = 5,
+        UserId = 5,
+        CategoryId = 5,
+        Title = "Hello Lola",
+        PublicationDate = new DateTime(2024, 6, 30),
+        ImageUrl = "https://i.imgur.com/bS6aYJ5.jpg",
+        Content = "Hello Lola, stop pooping on the bed",
+        Approved = false
+    },
 };
 
 
@@ -21,10 +65,23 @@ List<Comments> comments = new List<Comments>()
 {
     new Comments()
     {
-        Id = ,
-        AuthorId = ,
-        PostId Id = ,
-        Content = 
-
+        Id = 1,
+        AuthorId = 2,
+        PostId = 1,
+        Content = "Oh hi!",
+    },
+    new Comments()
+    {
+        Id = 2,
+        AuthorId = 3,
+        PostId = 5,
+        Content = "You shouldn't do that, Lola...",
+    },
+    new Comments()
+    {
+        Id = 3,
+        AuthorId = 4,
+        PostId = 4,
+        Content = "21!!!!!!!!!!!!! Cheers!",
     }
-}
+};
