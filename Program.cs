4cefe8067933afa04bc63ce9bbd0acda053f52ba
fmings <mingsfelicia@gmail.com>
2024-08-06 19:45:13 -0500

    starting mock data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using rare_BE.Models;
+
+List<Posts> posts = new List<Posts>()
+{
+    new Posts()
+    {
+        Id = 1,
+        UserId = 1,
+        CategoryId = 1,
+        Title = "Hello World",
+        PublicationDate = new DateTime(2024, 7, 19),
+        ImageUrl = "https://static1.squarespace.com/static/5e949a92e17d55230cd1d44f/t/61f35a8548933c4ce4cc0daa/1643338381475/HelloLight_Mac.png?format=1500w",
+        Content = "Hello World, how are you today?",
+        Approved = true
+    },
+};
+
+
+List<Comments> comments = new List<Comments>()
+{
+    new Comments()
+    {
+        Id = ,
+        AuthorId = ,
+        PostId Id = ,
+        Content = 
+
+    }
+}
