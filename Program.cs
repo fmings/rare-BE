765e2c34189086367cedfc66574b1cc5f435e2a1
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-08-06 20:54:42 -0500

    commiting changes from merge conflicts

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Data.Common;
-using System.Reflection.Emit;
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 using rare_BE.Models;
 
 List<Tags> tags = new List<Tags>
@@ -98,4 +100,35 @@ new PostReactions()
         UserId = 4,
         PostId = 5,
     },
+};
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+// Where APIs should begin
+app.MapGet("/test", () =>
+{
+    return "sugar";
+});
+
+app.Run();
