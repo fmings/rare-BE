b62b7c809acfcd8b44ddfb02b0205b341318e6f1
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-08-07 20:40:30 -0500

    Commiting changes to endpoint

--- Program.cs
+++ Program.cs
@@ -442,6 +442,29 @@ app.MapGet("/users/{id}/posts/{postId}", (int id, int postId) =>
     return Results.Ok(usersPost);
 });
 
+// Post PostReactions
+app.MapPost("/postreactions", (PostReactions postReaction) =>
+{
+    postReaction.Id = postReactions.Max(st => st.Id) + 1;
+    postReactions.Add(postReaction);
+    return postReaction;
+});
+
+app.MapGet("/reactions", () =>
+{
+    return reactions;
+});
+
+app.MapGet("/users", () =>
+{
+    return users;
+});
+
+app.MapGet("/postreactions", () =>
+{
+    return postReactions;
+});
+
 // GET ALL COMMENTS FOR A SPECIFIC USER POST
 app.MapGet("/post/{id}/comments", (int id) =>
 {
@@ -495,6 +518,37 @@ app.MapPost("/categories", (Categories newCategory) =>
 app.MapGet("/categories", () =>
 {
     return categories;
+});
+
+//Post Post Reactions
+app.MapPost("/postreactions", (PostReactions postReaction) =>
+{
+    postReaction.Id = postReactions.Max(pr => pr.Id) + 1;
+    postReactions.Add(postReaction);
+    return postReaction;
+});
+
+//Post Reactions
+app.MapPost("/reactions", (Reactions reaction) =>
+{
+    reaction.Id = reactions.Max(r => r.Id) + 1;
+    reactions.Add(reaction);
+    return reaction;
+});
+
+//Post Users
+app.MapPost("/users", (Users user) =>
+{
+    user.Id = users.Max(u => u.Id) + 1;
+    users.Add(user);
+    return user;
+});
+
+app.MapDelete("/postreactions/{id}", (int id) =>
+{
+    PostReactions? postReaction = postReactions.FirstOrDefault(pr => pr.Id == id);
+    int index = postReactions.IndexOf(postReaction);
+    postReactions.RemoveAt(index);
 });
 
 app.Run();
